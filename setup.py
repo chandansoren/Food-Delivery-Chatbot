8d9ea4b58ae1c02c79cf9796c6348837530e7c19
Chandan Soren <csoren66@gmail.com>
2024-10-04 15:31:00 +0530

    Chatbot

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = "Zomato Chatbot",
+    version= "0.0.0",
+    author= "Chandan Soren",
+    author_email= "sorenchandan82@gmail.com",
+    packages= find_packages(),
+    install_requires=[]
+)
